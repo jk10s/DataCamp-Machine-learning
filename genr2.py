19a727a957bc8629cb8f1313827aa5221638780b
jk10s <juanka_267@hotmail.com>
2022-08-08 23:22:36 -0500

    new file

---
+++ genr2.py
@@ -0,0 +1,5 @@
+# Print missing values for each column
+print(music_df.isna().sum().sort_values())
+
+# Remove values where less than 5% are missing
+music_df = music_df.dropna(subset=["genre", "popularity", "loudness", "liveness", "tempo"])
